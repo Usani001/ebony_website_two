dac3a5600924b34174433831304e061e98c3607a
Usani001 <emmanuel.usani@decagon.dev>
2023-09-25 16:47:50 +0100

    New commit

--- src/index.js
+++ src/index.js
@@ -8,12 +8,12 @@ import "./global.css";
 const container = document.getElementById("root");
 const root = createRoot(container);
 
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
-
+// root.render(
+//   <BrowserRouter>
+//     <App />
+//   </BrowserRouter>
+// );
+ReactDOM.render(<App />, document.getElementById("root"));
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
